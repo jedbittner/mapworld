6e16099792f8d54e67f2d0ebf090c1144d2975b2
jedbittner <jedbittner@gmail.com>
2017-07-20 11:48:08 -0700

    hard coded the size of squares; need to logically compute (1.7777778 ratio supported). - also setup random colors for map in prep for painting / saving / loading.

--- mapworld/Content/MapRenderer.cpp
+++ mapworld/Content/MapRenderer.cpp
@@ -64,10 +64,11 @@ void MapRenderer::CreateDeviceDependentResources()
 	Windows::Foundation::Size logicalSize = m_deviceResources->GetLogicalSize();
 	m_numberOfColumns = 40;
 	m_numberOfRows = 22;
-	m_leftRightEdges = 25;
-	m_topBottomEdges = m_leftRightEdges;
+	m_leftRightEdges = 40;
+	m_topBottomEdges = 32;
 	FLOAT modifiedWidth = logicalSize.Width - (m_leftRightEdges * 2);
-	m_side = modifiedWidth / m_numberOfColumns;
+//	m_side = modifiedWidth / m_numberOfColumns;
+	m_side = 38.0f;
 
 	Trace(L"logical size width %f\n", logicalSize.Width);
 	Trace(L"logical size height %f\n", logicalSize.Height);
@@ -81,15 +82,17 @@ void MapRenderer::CreateDeviceDependentResources()
 		m_deviceResources->GetD2DDeviceContext()->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::White), &m_solidColorBrush)
 		);
 
-	m_tileColors.push_back(D2D1::ColorF(220.0f / 255.0f, 62.0f / 255.0f, 35.0f / 255.0f, 1.0f));
-	m_tileColors.push_back(D2D1::ColorF(215.0f / 255.0f, 81.0f / 255.0f, 116.0f / 255.0f, 1.0f));
-	m_tileColors.push_back(D2D1::ColorF(235.0f / 255.0f, 91.0f / 255.0f, 57.0f / 124.0f, 1.0f));
-	m_tileColors.push_back(D2D1::ColorF(252.0f / 255.0f, 90.0f / 255.0f, 85.0f / 255.0f, 1.0f));
-	m_tileColors.push_back(D2D1::ColorF(215.0f / 255.0f, 81.0f / 255.0f, 116.0f / 255.0f, 1.0f));
-	m_tileColors.push_back(D2D1::ColorF(248.0f / 255.0f, 142.0f / 255.0f, 206.0f / 255.0f, 1.0f));
-	m_tileColors.push_back(D2D1::ColorF(119.0f / 255.0f, 160.0f / 255.0f, 81.0f / 255.0f, 1.0f));
-	m_tileColors.push_back(D2D1::ColorF(84.0f / 255.0f, 122.0f / 255.0f, 61.0f / 255.0f, 1.0f));
-	m_tileColors.push_back(D2D1::ColorF(80.0f / 255.0f, 115.0f / 255.0f, 61.0f / 255.0f, 1.0f));
+	srand(time(nullptr));	// seed random num generator
+	for (int i = 0; i < m_numberOfRows*m_numberOfColumns; i++)
+	{
+		FLOAT r;
+		FLOAT g;
+		FLOAT b;
+		r = (float)(rand() % 255);
+		g = (float)(rand() % 255);
+		b = (float)(rand() % 255);
+		m_tileColors.push_back(D2D1::ColorF(r / 255.0f, g / 255.0f, b / 255.0f, 1.0f));
+	}
 //	m_tileColors.push_back(D2D1::ColorF(1.0f, 0.0f, 0.0f, 1.0f));
 }
 void MapRenderer::ReleaseDeviceDependentResources()
